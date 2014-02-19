831665de00c8367d47964a707e55bf497441b6ca
maseek <tzemanovic@gmail.com>
2014-02-19 08:00:47 +0000

    initial setup

---
+++ protoedit/src/App.fs
@@ -0,0 +1,16 @@
+﻿module MainApp
+
+open System
+open System.Windows
+open System.Windows.Controls
+open FSharpx
+
+type MainWindow = XAML<"MainWindow.xaml">
+
+let loadWindow() =
+   let window = MainWindow()
+   
+   window.Root
+
+[<STAThread>]
+(new Application()).Run(loadWindow()) |> ignore
