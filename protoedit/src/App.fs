56f63884f310b94523a1394cd0aa64e72126ec96
maseek <tzemanovic@gmail.com>
2014-02-22 19:28:42 +0000

    file picking GUI - drag&drop

--- protoedit/src/App.fs
+++ protoedit/src/App.fs
@@ -3,14 +3,30 @@
 open System
 open System.Windows
 open System.Windows.Controls
+open System.Windows.Media
 open FSharpx
 
 type MainWindow = XAML<"src/MainWindow.xaml">
 
+let previewDrag (e : DragEventArgs) =
+    e.Effects <- DragDropEffects.Link
+    e.Handled <- true
+
+let previewDrop (e : DragEventArgs) =
+    let filePath : string [] = e.Data.GetData DataFormats.FileDrop |> unbox
+    let source : TextBox = e.Source |> unbox
+    source.Text <- String.Format("{0}", filePath.[0])
+
 let loadWindow() =
-   let window = MainWindow()
-   
-   window.Root
+    let window = MainWindow()
+    let protoInput : TextBox = window.Root.FindName "protoInput" |> unbox
+    protoInput.PreviewDragOver.Add(previewDrag)
+    protoInput.PreviewDrop.Add(previewDrop)
+    let dataInput : TextBox = window.Root.FindName "dataInput" |> unbox
+    dataInput.PreviewDragOver.Add(previewDrag)
+    dataInput.PreviewDrop.Add(previewDrop)
+    window.Root
+
 
 [<STAThread>]
 (new Application()).Run(loadWindow()) |> ignore
---
+++ protoedit/src/core/ProtoDescriptor.fs
@@ -0,0 +1,46 @@
+﻿module ProtoDescriptor
+
+type FieldType = 
+    | TypeDouble = 1
+    | TypeFloat = 2
+    | TypeInt64 = 3
+    | TypeUint64 = 4
+    | TypeInt32 = 5
+    | TypeFixed64 = 6
+    | TypeFixed32 = 7
+    | TypeBool = 8
+    | TypeString = 9
+    | TypeGroup = 10
+    | TypeMessage = 11
+    | TypeBytes = 12
+    | TypeUint32 = 13
+    | TypeEnum = 14
+    | TypeSFixed32 = 15
+    | TypeSFixed64 = 16
+    | TypeSInt32 = 17
+    | TypeSInt64 = 18
+
+type FieldLabel =
+    | LabelOptional = 1
+    | LabelRequired = 2
+    | LabelRepeated = 3
+
+type FieldDescriptor =
+    {Name : string;
+    Number : int;
+    Type : FieldType;
+    Label : FieldLabel;}
+
+type EnumValueDescriptor =
+    {Name : string;
+    Number : int}
+
+type EnumDescriptor =
+    {Name : string;
+    Values : EnumValueDescriptor}
+
+type ProtoDescriptor = 
+    {Name : string;
+    Children : ProtoDescriptor;
+    Fields : FieldDescriptor;
+    Enums : EnumDescriptor}
